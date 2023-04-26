044d1eeb3f901644a98a2a3393c481fce481c965
yfranz <yfranz@gmail.com>
2023-04-25 19:54:08 -0700

    Step 4: Implement a mock for this service.

--- MyBlazorApp.Mocks/ServiceExtensions.cs
+++ MyBlazorApp.Mocks/ServiceExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MyBlazorApp.Mocks.ServicesMocks;
+using MyBlazorApp.Services.Api;
 
 public static class ServiceExtensions
 {
     public static IServiceCollection AddMockAppServices(this IServiceCollection services)
     {
-        //services.AddScoped<Interface, Implementation>();
+        services.AddScoped<IWeatherForecast, WeatherForecastMock>();
 
         return services;
     }
---
+++ MyBlazorApp.Mocks/ServicesMocks/WeatherForecastMock.cs
@@ -0,0 +1,22 @@
+﻿using MyBlazorApp.Services.Api;
+using MyBlazorApp.Shared;
+
+namespace MyBlazorApp.Mocks.ServicesMocks;
+
+internal class WeatherForecastMock : IWeatherForecast
+{
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public async Task<WeatherForecast[]> GetForecastAsync()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+    }
+}
